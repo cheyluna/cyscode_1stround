9663c1f560b287c79b07ab946586a2bcbaa38c97
Chey Luna <rachel.luna@klab.com>
2014-09-30 14:47:29 +0800

    Added hasDuplicates() and covertTotString() methods in models/chocolate.php.

--- app/models/chocolate.php
+++ app/models/chocolate.php
@@ -3,6 +3,8 @@ class Chocolate extends AppModel
 {
     const MIN_STRING_LENGTH = 1;
     const MAX_STRING_LENGTH = 50;
+    const ONE_CHARACTER = 1;
+    const SINGLE_INSTANCE = 1;
 
     public $validation = array(
         'raw_string' => array (
@@ -25,16 +27,40 @@ class Chocolate extends AppModel
     public function getRemainingBars($raw_string)
     {
         $array = $this->convertToArray($raw_string);
+
+        while ($this->hasDuplicates($array)) {
+            array_pop($array);
+        }
+
         $remaining_bars = $this->getArrayCount($array);
+
         return $remaining_bars;
     }
 
-    public function convertToArray($raw_string)
+    private function convertToArray($raw_string)
     {
         return str_split($raw_string);
     }
 
-    public function getArrayCount($string_array)
+    private function hasDuplicates($array)
+    {
+        $temp_string = $this->convertToString($array);
+
+        foreach (count_chars($temp_string, self::ONE_CHARACTER) as $v) {
+            if ($v > self::SINGLE_INSTANCE) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function convertToString($array)
+    {
+        return implode('', $array);
+    }
+
+    private function getArrayCount($string_array)
     {
         return count($string_array);
     }
