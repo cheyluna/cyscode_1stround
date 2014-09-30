823fd293debf41116c2524e9306392074c2abf3c
Chey Luna <rachel.luna@klab.com>
2014-09-30 13:49:18 +0800

    Added call to Chocolate::getRemainingBars() in chocolate_controller.php.

--- app/controllers/chocolate_controller.php
+++ app/controllers/chocolate_controller.php
@@ -3,12 +3,14 @@ class ChocolateController extends AppController
 {
     public function index()
     {
+        $remaining_bars = 0;
         $choco = new Chocolate;
         $choco->raw_string = Param::get('raw_string');
 
         if (isset($_POST['submit'])) {
             try {
                 $choco->validateString($choco);
+                $remaining_bars = $choco->getRemainingBars($choco->raw_string);
             } catch (ValidationException $e) {
                 // Do something here
             }
